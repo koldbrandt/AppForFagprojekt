baf52f6220dc499038ce7c9011877ecbb1d43bc4
Kjær <crhk1998@gmail.com>
2020-06-08 12:50:32 +0200

    Tilføjet logikken bag indkommende beskeder fra server til at opdaterer de rigtige felter.

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -6,23 +6,27 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
     Button NewKeyButton;
-    TextView message;
+    TextView SignalText;
+    TextView BitErrorText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        message = (TextView)findViewById(R.id.BitRateText);
+        SignalText = (TextView)findViewById(R.id.SignalText);
+        SignalText.setText(0);
+        BitErrorText = (TextView)findViewById(R.id.BitErrorText);
+        BitErrorText.setText(0);
         NewKeyButton = (Button)findViewById(R.id.NewKeyButton);
         Thread myThread = new Thread(new MyReadThread());
         myThread.start();
@@ -53,18 +57,42 @@ public class MainActivity extends AppCompatActivity {
                 while(true){
                     mes = br.readLine();
 
-                    h.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            message.setText(mes);
-                        }
-                    });
+                    String code = mes.substring(0,8);
+                    final String value = mes.substring(8);
+                    switch(code){
+                        case "00000001" :
+                            h.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    SignalText.setText(value);
+                                }
+                            });
+                            break;
+                        case "00000010" :
+                            h.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    BitErrorText.setText(value);
+                                }
+                            });
+                            break;
+                        case "00000011" :
+                            h.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(), "Encryptio Key Changed", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                            break;
+
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
+
 
     public void requestNewKey(View v){
         MessageSender ms = new MessageSender();
